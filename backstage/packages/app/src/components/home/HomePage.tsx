b094638289aecd8f6141cfb7ee02960e1da5dabb
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2023-02-27 15:19:46 +0000

    Add developer guide card to homepage

--- backstage/packages/app/src/components/home/HomePage.tsx
+++ backstage/packages/app/src/components/home/HomePage.tsx
@@ -9,6 +9,7 @@ import {
   Typography,
 } from '@material-ui/core';
 import CategoryIcon from '@material-ui/icons/Category';
+import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
 import React from 'react';
 
 const SoftwareCatalogCard = () => {
@@ -16,18 +17,42 @@ const SoftwareCatalogCard = () => {
     useApi(configApiRef).getOptionalString('organization.name') ?? 'Backstage';
 
   return (
-    <Card>
+    <Card style={{ height: '100%' }}>
       <CardContent>
         <Typography variant="h5">
           <CategoryIcon /> Software Catalog
         </Typography>
         <Typography variant="body2">
           A central location to discover software components developed at{' '}
-          {orgName}
+          {orgName}.
         </Typography>
       </CardContent>
       <CardActions>
         <Button size="medium" color="primary" href="/catalog">
+          Visit
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+const DeveloperGuideCard = () => {
+  const orgName =
+    useApi(configApiRef).getOptionalString('organization.name') ?? 'Backstage';
+
+  return (
+    <Card style={{ height: '100%' }}>
+      <CardContent>
+        <Typography variant="h5">
+          <LibraryBooksIcon /> Developer Guide
+        </Typography>
+        <Typography variant="body2">
+          Extensive documentation on developing software at {orgName}. Including
+          Tutorials, How-Tos, Explanations & References.
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="medium" color="primary" href="/guide">
           Visit
         </Button>
       </CardActions>
@@ -42,9 +67,17 @@ export const HomePage = () => {
   return (
     <PageWithHeader title={`${orgName} Developer Portal`} themeId="home">
       <Content>
-        <Grid justifyContent="center" spacing={6}>
+        <Grid
+          container
+          justifyContent="center"
+          spacing={6}
+          alignItems="stretch"
+        >
           <Grid item xs={12} md={6} lg={4}>
             <SoftwareCatalogCard />
+          </Grid>
+          <Grid item xs={12} md={6} lg={4}>
+            <DeveloperGuideCard />
           </Grid>
         </Grid>
       </Content>
