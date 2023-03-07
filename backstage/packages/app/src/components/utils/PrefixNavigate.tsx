cee03ca9eafd9be09aa5557aa8e3b70743d5ba96
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2023-03-07 15:00:59 +0000

    Create prefix navigation util

---
+++ backstage/packages/app/src/components/utils/PrefixNavigate.tsx
@@ -0,0 +1,53 @@
+import { UNSAFE_invariant, warning } from '@remix-run/router';
+import { useContext, useEffect } from 'react';
+import {
+  RelativeRoutingType,
+  UNSAFE_DataRouterStateContext,
+  UNSAFE_NavigationContext,
+  useInRouterContext,
+  useLocation,
+  useNavigate,
+} from 'react-router-dom';
+
+export interface PrefixNavigateProps {
+  fromPrefix: string;
+  toPrefix: string;
+  replace?: boolean;
+  state?: any;
+  relative?: RelativeRoutingType;
+}
+
+export function PrefixNavigate({
+  fromPrefix,
+  toPrefix,
+  replace,
+  state,
+  relative,
+}: PrefixNavigateProps): null {
+  // eslint-disable-next-line new-cap
+  UNSAFE_invariant(
+    useInRouterContext(),
+    `<NavigateRelative> may be used only in the context of a <Router> component.`,
+  );
+
+  warning(
+    !useContext(UNSAFE_NavigationContext).static,
+    `<NavigateRelative> must not be used on the initial render in a <StaticRouter>. ` +
+      `This is a no-op, but you should modify your code so the <NavigateRelative> is ` +
+      `only ever rendered in response to some user interaction or state change.`,
+  );
+
+  const dataRouterState = useContext(UNSAFE_DataRouterStateContext);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const to = location.pathname.replace(fromPrefix, toPrefix);
+
+  useEffect(() => {
+    if (dataRouterState && dataRouterState.navigation.state !== 'idle') {
+      return;
+    }
+    navigate(to, { replace, state, relative });
+  });
+
+  return null;
+}
