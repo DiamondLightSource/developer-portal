025b5f4f362a8200ffd26c075dadfe0edbaeb0fb
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2023-03-23 17:26:28 +0000

    Remove techdocs link from about card

---
+++ backstage/packages/app/src/components/AboutCard/AboutCard.tsx
@@ -0,0 +1,153 @@
+import {
+  ANNOTATION_EDIT_URL,
+  ANNOTATION_LOCATION,
+  stringifyEntityRef,
+} from '@backstage/catalog-model';
+import {
+  HeaderIconLinkRow,
+  IconLinkVerticalProps,
+  InfoCardVariants,
+  Link,
+} from '@backstage/core-components';
+import {
+  alertApiRef,
+  ErrorApiError,
+  errorApiRef,
+  useApi,
+} from '@backstage/core-plugin-api';
+import {
+  ScmIntegrationIcon,
+  scmIntegrationsApiRef,
+} from '@backstage/integration-react';
+import { AboutContent } from '@backstage/plugin-catalog';
+import {
+  catalogApiRef,
+  getEntitySourceLocation,
+  useEntity,
+} from '@backstage/plugin-catalog-react';
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Divider,
+  IconButton,
+  makeStyles,
+} from '@material-ui/core';
+import CachedIcon from '@material-ui/icons/Cached';
+import EditIcon from '@material-ui/icons/Edit';
+import React, { useCallback } from 'react';
+
+const useStyles = makeStyles({
+  gridItemCard: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: 'calc(100% - 10px)', // for pages without content header
+    marginBottom: '10px',
+  },
+  fullHeightCard: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+  },
+  gridItemCardContent: {
+    flex: 1,
+  },
+  fullHeightCardContent: {
+    flex: 1,
+  },
+});
+
+/**
+ * Props for {@link EntityAboutCard}.
+ *
+ * @public
+ */
+export interface AboutCardProps {
+  variant?: InfoCardVariants;
+}
+
+export function EntityAboutCard(props: AboutCardProps) {
+  const { variant } = props;
+  const classes = useStyles();
+  const { entity } = useEntity();
+  const scmIntegrationsApi = useApi(scmIntegrationsApiRef);
+  const catalogApi = useApi(catalogApiRef);
+  const alertApi = useApi(alertApiRef);
+  const errorApi = useApi(errorApiRef);
+
+  const entitySourceLocation = getEntitySourceLocation(
+    entity,
+    scmIntegrationsApi,
+  );
+  const entityMetadataEditUrl =
+    entity.metadata.annotations?.[ANNOTATION_EDIT_URL];
+
+  const viewInSource: IconLinkVerticalProps = {
+    label: 'View Source',
+    disabled: !entitySourceLocation,
+    icon: <ScmIntegrationIcon type={entitySourceLocation?.integrationType} />,
+    href: entitySourceLocation?.locationTargetUrl,
+  };
+
+  let cardClass = '';
+  if (variant === 'gridItem') {
+    cardClass = classes.gridItemCard;
+  } else if (variant === 'fullHeight') {
+    cardClass = classes.fullHeightCard;
+  }
+
+  let cardContentClass = '';
+  if (variant === 'gridItem') {
+    cardContentClass = classes.gridItemCardContent;
+  } else if (variant === 'fullHeight') {
+    cardContentClass = classes.fullHeightCardContent;
+  }
+
+  const entityLocation = entity.metadata.annotations?.[ANNOTATION_LOCATION];
+  // Limiting the ability to manually refresh to the less expensive locations
+  const allowRefresh =
+    entityLocation?.startsWith('url:') || entityLocation?.startsWith('file:');
+  const refreshEntity = useCallback(async () => {
+    try {
+      await catalogApi.refreshEntity(stringifyEntityRef(entity));
+      alertApi.post({ message: 'Refresh scheduled', severity: 'info' });
+    } catch (e) {
+      errorApi.post(e as ErrorApiError);
+    }
+  }, [catalogApi, alertApi, errorApi, entity]);
+
+  return (
+    <Card className={cardClass}>
+      <CardHeader
+        title="About"
+        action={
+          <>
+            {allowRefresh && (
+              <IconButton
+                aria-label="Refresh"
+                title="Schedule entity refresh"
+                onClick={refreshEntity}
+              >
+                <CachedIcon />
+              </IconButton>
+            )}
+            <IconButton
+              component={Link}
+              aria-label="Edit"
+              disabled={!entityMetadataEditUrl}
+              title="Edit Metadata"
+              to={entityMetadataEditUrl ?? '#'}
+            >
+              <EditIcon />
+            </IconButton>
+          </>
+        }
+        subheader={<HeaderIconLinkRow links={[viewInSource]} />}
+      />
+      <Divider />
+      <CardContent className={cardContentClass}>
+        <AboutContent entity={entity} />
+      </CardContent>
+    </Card>
+  );
+}
