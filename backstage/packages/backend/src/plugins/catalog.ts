9304f35fd018c5cc2a45ba289fc313331b04eaa9
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2023-05-31 13:03:42 +0000

    Add openapi backend plugin

--- backstage/packages/backend/src/plugins/catalog.ts
+++ backstage/packages/backend/src/plugins/catalog.ts
@@ -2,6 +2,7 @@ import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
 import { GithubEntityProvider } from '@backstage/plugin-catalog-backend-module-github';
 import { GitlabDiscoveryEntityProvider } from '@backstage/plugin-catalog-backend-module-gitlab';
 import { LdapOrgEntityProvider } from '@backstage/plugin-catalog-backend-module-ldap';
+import { jsonSchemaRefPlaceholderResolver } from '@backstage/plugin-catalog-backend-module-openapi';
 import { ScaffolderEntitiesProcessor } from '@backstage/plugin-scaffolder-backend';
 import { GitlabFillerProcessor } from '@immobiliarelabs/backstage-plugin-gitlab-backend';
 import { Router } from 'express';
@@ -36,6 +37,8 @@ export default async function createPlugin(
   );
   builder.addProcessor(new ScaffolderEntitiesProcessor());
   builder.addProcessor(new GitlabFillerProcessor(env.config));
+  builder.setPlaceholderResolver('openapi', jsonSchemaRefPlaceholderResolver);
+  builder.setPlaceholderResolver('asyncapi', jsonSchemaRefPlaceholderResolver);
   const { processingEngine, router } = await builder.build();
   await processingEngine.start();
   return router;
