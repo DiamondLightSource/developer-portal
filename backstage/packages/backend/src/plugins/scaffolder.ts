0bfff0d2593d7286c3cab38814412293f6bcf0df
Man Ting Chan <man.chan@diamond.ac.uk>
2023-12-11 09:43:29 +0000

    Added dependencies and support for Copier
    added copier to install requirements
    
    Added dependencies and support for copier plugin
    
    added package dependency
    
    for backstage plugin and copier compatibility
    
    added missing dependency

--- backstage/packages/backend/src/plugins/scaffolder.ts
+++ backstage/packages/backend/src/plugins/scaffolder.ts
@@ -1,5 +1,13 @@
+// packages/backend/src/plugins/scaffolder.ts
+
+import { DockerContainerRunner } from '@backstage/backend-common';
 import { CatalogClient } from '@backstage/catalog-client';
-import { createRouter } from '@backstage/plugin-scaffolder-backend';
+import { ScmIntegrations } from '@backstage/integration';
+import { createBuiltinActions, createRouter } from '@backstage/plugin-scaffolder-backend';
+import { createFetchCopierAction } from '@backstage/plugin-scaffolder-backend-module-copier';
+import Docker from 'dockerode';
+
+
 import { Router } from 'express';
 import type { PluginEnvironment } from '../types';
 
@@ -9,13 +17,27 @@ export default async function createPlugin(
   const catalogClient = new CatalogClient({
     discoveryApi: env.discovery,
   });
+  const integrations = ScmIntegrations.fromConfig(env.config);
+  const dockerClient = new Docker();
+  const containerRunner = new DockerContainerRunner({ dockerClient });
 
+  const builtInActions = createBuiltinActions({
+    integrations,
+    catalogClient,
+    config: env.config,
+    reader: env.reader,
+  });
+  const actions = [...builtInActions, createFetchCopierAction({integrations, reader: env.reader, containerRunner})];
   return await createRouter({
+    containerRunner,
+    catalogClient,
+    actions,
     logger: env.logger,
     config: env.config,
     database: env.database,
     reader: env.reader,
-    catalogClient,
     identity: env.identity,
+    scheduler: env.scheduler,
   });
 }
+
