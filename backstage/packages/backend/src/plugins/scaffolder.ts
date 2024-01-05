cfff4d039ddc2623e780a49a09bba75fe296bd6f
Man Ting Chan <man.chan@diamond.ac.uk>
2024-01-05 13:49:31 +0000

    Add support for copier plugin including associated dependencies
    install copier
    
    added copier plugin

--- backstage/packages/backend/src/plugins/scaffolder.ts
+++ backstage/packages/backend/src/plugins/scaffolder.ts
@@ -1,11 +1,10 @@
-// packages/backend/src/plugins/scaffolder.ts
-
 import { CatalogClient } from '@backstage/catalog-client';
 import { ScmIntegrations } from '@backstage/integration';
-import { createBuiltinActions, createRouter } from '@backstage/plugin-scaffolder-backend';
+import {
+  createBuiltinActions,
+  createRouter,
+} from '@backstage/plugin-scaffolder-backend';
 import { createFetchCopierAction } from '@backstage/plugin-scaffolder-backend-module-copier';
-
-
 import { Router } from 'express';
 import type { PluginEnvironment } from '../types';
 
@@ -17,13 +16,16 @@ export default async function createPlugin(
   });
   const integrations = ScmIntegrations.fromConfig(env.config);
 
-  const builtInActions = createBuiltinActions({
-    integrations,
-    catalogClient,
-    config: env.config,
-    reader: env.reader,
-  });
-  const actions = [...builtInActions, createFetchCopierAction({integrations, reader: env.reader})];
+  const actions = [
+    ...createBuiltinActions({
+      integrations,
+      catalogClient,
+      config: env.config,
+      reader: env.reader,
+    }),
+    createFetchCopierAction({ integrations, reader: env.reader }),
+  ];
+
   return await createRouter({
     catalogClient,
     actions,
