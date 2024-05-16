1558bfe080e490c4e7d68af8ec57cbbb8e673f05
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-05-16 13:46:52 +0000

    format

--- backstage/packages/app/src/components/dev-guide/GuideDocsPage.tsx
+++ backstage/packages/app/src/components/dev-guide/GuideDocsPage.tsx
@@ -1,24 +1,18 @@
 import { PageWithHeader } from '@backstage/core-components';
+import {
+  useRouteRefParams
+} from '@backstage/core-plugin-api';
 import { TechDocsReaderPageContent } from '@backstage/plugin-techdocs';
 import { TechDocsReaderPageProvider } from '@backstage/plugin-techdocs-react';
 import React from 'react';
-import { rootDocsRouteRef } from '../../../routes';
-import {
-  getComponentData,
-  useRouteRefParams,
-} from '@backstage/core-plugin-api';
+import { rootDocsRouteRef } from '../../routes';
 
 // based on this https://github.com/backstage/backstage/blob/master/plugins/techdocs/src/reader/components/TechDocsReaderPage/TechDocsReaderPage.tsx
 export const DeveloperGuidePage = () => {
-    const { kind, name, namespace } = useRouteRefParams(rootDocsRouteRef);
-  const entityRef = {kind, name, namespace}
-  const oldEntityRef={
-        kind: 'component',
-        namespace: 'default',
-        name: 'developer-guide',
-      }
+  const { kind, name, namespace } = useRouteRefParams(rootDocsRouteRef);
+  const entityRef = { kind, name, namespace }
 
-   return <PageWithHeader title={name} themeId="documentation">
+  return <PageWithHeader title={name} themeId="diamond-theme">
     <TechDocsReaderPageProvider entityRef={entityRef} >
       <TechDocsReaderPageContent />
     </TechDocsReaderPageProvider>
