09810794056ac78d7fff419086636921316e70e6
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-05-16 13:44:51 +0000

    format files

--- backstage/packages/app/src/App.tsx
+++ backstage/packages/app/src/App.tsx
@@ -37,11 +37,11 @@ import { HomePage } from './components/home/HomePage';
 import { PortalDocsPage } from './components/portal-docs/PortalDocsPage';
 import { searchPage } from './components/search/SearchPage';
 import { PrefixNavigate } from './components/utils/PrefixNavigate';
-import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import LightIcon from '@material-ui/icons/WbSunny';
-import { diamondTheme } from './themes/diamondTheme.ts'
-import { testTheme } from './themes/testTheme.ts'
+import { diamondTheme } from './themes/diamondTheme'
+import { testTheme } from './themes/testTheme'
+import { UnifiedThemeProvider } from '@backstage/theme';
 
 const app = createApp({
   apis,
@@ -61,25 +61,25 @@ const app = createApp({
     });
   },
 
-  themes:[
+  themes: [
     {
-    id:'diamond-theme',
-    title: 'Diamond Theme',
-    variant: 'light',
-    icon: <LightIcon/>,
-    Provider: ({children})=>(
-      <UnifiedThemeProvider theme={diamondTheme} children={children}/>
-    )
-  },
+      id: 'diamond-theme',
+      title: 'Diamond Theme',
+      variant: 'light',
+      icon: <LightIcon />,
+      Provider: ({ children }) => (
+        <UnifiedThemeProvider theme={diamondTheme} children={children} />
+      )
+    },
     {
-    id:'test-theme',
-    title: 'Test Theme',
-    variant: 'light',
-    icon: <p> test</p>,
-    Provider: ({children})=>(
-      <UnifiedThemeProvider theme={testTheme} children={children}/>
-    )
-  },
+      id: 'test-theme',
+      title: 'Test Theme',
+      variant: 'light',
+      icon: <p> test</p>,
+      Provider: ({ children }) => (
+        <UnifiedThemeProvider theme={testTheme} children={children} />
+      )
+    },
   ]
 });
 
--- backstage/packages/app/src/themes/testTheme.ts
+++ backstage/packages/app/src/themes/testTheme.ts
@@ -7,48 +7,48 @@ import {
 } from '@backstage/theme';
 
 const testPalette = {
-      primary: {
-        main: '#343b58',
-      },
-      secondary: {
-        main: '#565a6e',
-      },
-      error: {
-        main: '#8c4351',
-      },
-      warning: {
-        main: '#8f5e15',
-      },
-      info: {
-        main: '#34548a',
-      },
-      success: {
-        main: '#485e30',
-      },
-      background: {
-        default: '#d5d6db',
-        paper: '#d5d6db',
-      },
-      banner: {
-        info: '#34548a',
-        error: '#8c4351',
-        text: '#343b58',
-        link: '#565a6e',
-      },
-      errorBackground: '#8c4351',
-      warningBackground: '#8f5e15',
-      infoBackground: '#343b58',
-      navigation: {
-        background: '#343b58',
-        indicator: '#8f5e15',
-        color: '#d5d6db',
-        selectedColor: '#ffffff',
-      },
-    };
+  primary: {
+    main: '#343b58',
+  },
+  secondary: {
+    main: '#565a6e',
+  },
+  error: {
+    main: '#8c4351',
+  },
+  warning: {
+    main: '#8f5e15',
+  },
+  info: {
+    main: '#34548a',
+  },
+  success: {
+    main: '#485e30',
+  },
+  background: {
+    default: '#d5d6db',
+    paper: '#d5d6db',
+  },
+  banner: {
+    info: '#34548a',
+    error: '#8c4351',
+    text: '#343b58',
+    link: '#565a6e',
+  },
+  errorBackground: '#8c4351',
+  warningBackground: '#8f5e15',
+  infoBackground: '#343b58',
+  navigation: {
+    background: '#343b58',
+    indicator: '#8f5e15',
+    color: '#d5d6db',
+    selectedColor: '#ffffff',
+  },
+};
 
 export const testTheme = createUnifiedTheme({
   ...createBaseThemeOptions({
-    palette: { ...palettes.light, ...testPalette}
+    palette: { ...palettes.light, ...testPalette },
   }),
   defaultPageTheme: 'home',
   fontFamily: 'Comic Sans MS',
