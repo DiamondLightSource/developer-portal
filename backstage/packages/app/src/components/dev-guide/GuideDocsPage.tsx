e071a2909f1eb81908f42c7b7b8587384c38bd2e
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-05-20 10:52:52 +0100

    fix lint in guidedocs page

--- backstage/packages/app/src/components/dev-guide/GuideDocsPage.tsx
+++ backstage/packages/app/src/components/dev-guide/GuideDocsPage.tsx
@@ -1,7 +1,5 @@
 import { PageWithHeader } from '@backstage/core-components';
-import {
-  useRouteRefParams
-} from '@backstage/core-plugin-api';
+import { useRouteRefParams } from '@backstage/core-plugin-api';
 import { TechDocsReaderPageContent } from '@backstage/plugin-techdocs';
 import { TechDocsReaderPageProvider } from '@backstage/plugin-techdocs-react';
 import React from 'react';
@@ -10,11 +8,13 @@ import { rootDocsRouteRef } from '../../routes';
 // based on this https://github.com/backstage/backstage/blob/master/plugins/techdocs/src/reader/components/TechDocsReaderPage/TechDocsReaderPage.tsx
 export const DeveloperGuidePage = () => {
   const { kind, name, namespace } = useRouteRefParams(rootDocsRouteRef);
-  const entityRef = { kind, name, namespace }
+  const entityRef = { kind, name, namespace };
 
-  return <PageWithHeader title={name} themeId="diamond-theme">
-    <TechDocsReaderPageProvider entityRef={entityRef} >
-      <TechDocsReaderPageContent />
-    </TechDocsReaderPageProvider>
-  </PageWithHeader>
+  return (
+    <PageWithHeader title={name} themeId="diamond-theme">
+      <TechDocsReaderPageProvider entityRef={entityRef}>
+        <TechDocsReaderPageContent />
+      </TechDocsReaderPageProvider>
+    </PageWithHeader>
+  );
 };
