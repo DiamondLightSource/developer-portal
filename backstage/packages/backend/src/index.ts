aa4e12cb65254bef632dcbb99558739ee30a7a02
Gary Yendell <gary.yendell@diamond.ac.uk>
2025-01-24 16:08:38 +0000

    Migrate to new backend

--- backstage/packages/backend/src/index.ts
+++ backstage/packages/backend/src/index.ts
@@ -1,107 +1,13 @@
-import {
-  CacheManager,
-  createServiceBuilder,
-  DatabaseManager,
-  getRootLogger,
-  loadBackendConfig,
-  notFoundHandler,
-  ServerTokenManager,
-  SingleHostDiscovery,
-  useHotMemoize,
-} from '@backstage/backend-common';
-import { UrlReaders } from '@backstage/backend-defaults/urlReader';
-import { TaskScheduler } from '@backstage/backend-tasks';
-import { Config } from '@backstage/config';
-import { DefaultIdentityClient } from '@backstage/plugin-auth-node';
-import { ServerPermissionClient } from '@backstage/plugin-permission-node';
-import Router from 'express-promise-router';
-import auth from './plugins/auth';
-import catalog from './plugins/catalog';
-import gitlab from './plugins/gitlab';
-import proxy from './plugins/proxy';
-import scaffolder from './plugins/scaffolder';
-import search from './plugins/search';
-import techdocs from './plugins/techdocs';
-import { PluginEnvironment } from './types';
+import { legacyPlugin } from '@backstage/backend-common';
+import { createBackend } from '@backstage/backend-defaults';
 
-function makeCreateEnv(config: Config) {
-  const root = getRootLogger();
-  const reader = UrlReaders.default({ logger: root, config });
-  const discovery = SingleHostDiscovery.fromConfig(config);
-  const cacheManager = CacheManager.fromConfig(config);
-  const databaseManager = DatabaseManager.fromConfig(config, { logger: root });
-  const tokenManager = ServerTokenManager.noop();
-  const taskScheduler = TaskScheduler.fromConfig(config);
+const backend = createBackend();
+backend.add(legacyPlugin('auth', import('./plugins/auth')));
+backend.add(legacyPlugin('catalog', import('./plugins/catalog')));
+backend.add(legacyPlugin('gitlab', import('./plugins/gitlab')));
+backend.add(legacyPlugin('proxy', import('./plugins/proxy')));
+backend.add(legacyPlugin('scaffolder', import('./plugins/scaffolder')));
+backend.add(legacyPlugin('search', import('./plugins/search')));
+backend.add(legacyPlugin('techdocs', import('./plugins/techdocs')));
 
-  const identity = DefaultIdentityClient.create({
-    discovery,
-  });
-  const permissions = ServerPermissionClient.fromConfig(config, {
-    discovery,
-    tokenManager,
-  });
-
-  root.info(`Created UrlReader ${reader}`);
-
-  return (plugin: string): PluginEnvironment => {
-    const logger = root.child({ type: 'plugin', plugin });
-    const database = databaseManager.forPlugin(plugin);
-    const cache = cacheManager.forPlugin(plugin);
-    const scheduler = taskScheduler.forPlugin(plugin);
-    return {
-      logger,
-      database,
-      cache,
-      config,
-      reader,
-      discovery,
-      tokenManager,
-      scheduler,
-      permissions,
-      identity,
-    };
-  };
-}
-
-async function main() {
-  const config = await loadBackendConfig({
-    argv: process.argv,
-    logger: getRootLogger(),
-  });
-  const createEnv = makeCreateEnv(config);
-
-  const catalogEnv = useHotMemoize(module, () => createEnv('catalog'));
-  const scaffolderEnv = useHotMemoize(module, () => createEnv('scaffolder'));
-  const authEnv = useHotMemoize(module, () => createEnv('auth'));
-  const proxyEnv = useHotMemoize(module, () => createEnv('proxy'));
-  const techdocsEnv = useHotMemoize(module, () => createEnv('techdocs'));
-  const searchEnv = useHotMemoize(module, () => createEnv('search'));
-  const gitlabEnv = useHotMemoize(module, () => createEnv('gitlab'));
-
-  const apiRouter = Router();
-  apiRouter.use('/catalog', await catalog(catalogEnv));
-  apiRouter.use('/scaffolder', await scaffolder(scaffolderEnv));
-  apiRouter.use('/auth', await auth(authEnv));
-  apiRouter.use('/techdocs', await techdocs(techdocsEnv));
-  apiRouter.use('/proxy', await proxy(proxyEnv));
-  apiRouter.use('/search', await search(searchEnv));
-  apiRouter.use('/gitlab', await gitlab(gitlabEnv));
-
-  // Add backends ABOVE this line; this 404 handler is the catch-all fallback
-  apiRouter.use(notFoundHandler());
-
-  const service = createServiceBuilder(module)
-    .loadConfig(config)
-    .addRouter('/api', apiRouter);
-
-  await service.start().catch(err => {
-    console.log(err);
-    process.exit(1);
-  });
-}
-
-module.hot?.accept();
-main().catch(error => {
-  console.error('Backend failed to start up', error);
-  process.exit(1);
-});
+backend.start();
