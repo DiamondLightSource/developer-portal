d9ae9e18357474690f83859cbf17175fbabff25e
Gary Yendell <gary.yendell@diamond.ac.uk>
2025-01-27 11:23:32 +0000

    Update GitLab plugin for new backend
    https://github.com/immobiliare/backstage-plugin-gitlab?tab=readme-ov-file#register-to-the-new-backend-system

--- backstage/packages/backend/src/index.ts
+++ backstage/packages/backend/src/index.ts
@@ -1,10 +1,17 @@
 import { legacyPlugin } from '@backstage/backend-common';
 import { createBackend } from '@backstage/backend-defaults';
+import {
+  catalogPluginGitlabFillerProcessorModule,
+  gitlabPlugin,
+} from '@immobiliarelabs/backstage-plugin-gitlab-backend';
 
 const backend = createBackend();
 backend.add(legacyPlugin('auth', import('./plugins/auth')));
 backend.add(legacyPlugin('catalog', import('./plugins/catalog')));
-backend.add(legacyPlugin('gitlab', import('./plugins/gitlab')));
+
+// GitLab
+backend.add(gitlabPlugin);
+backend.add(catalogPluginGitlabFillerProcessorModule);
 backend.add(legacyPlugin('proxy', import('./plugins/proxy')));
 backend.add(legacyPlugin('scaffolder', import('./plugins/scaffolder')));
 backend.add(legacyPlugin('search', import('./plugins/search')));
